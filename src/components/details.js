b5792f12dde89f425b455ebd2b0ef5e07a9a09d0
Prajwal <prajwalvernekar96@gmail.com>
2024-09-24 21:04:45 +0530

    Split components into pages and some fixes . NF

--- src/components/details.js
+++ src/components/details.js
@@ -1,8 +1,7 @@
 import React from "react";
-import Listitem from "../components/listitem";
-const DetailsPage = ({ match}) => {
+const DetailsPage = ({ match,items}) => {
   const itemId = match.params.id;
-  const item = match112.find((item) => item.id === itemId);
+  const item = items.find((item) => item.id === itemId);
 
   if (!item) {
     return <p>Item not found</p>;
@@ -11,7 +10,6 @@ const DetailsPage = ({ match}) => {
   return (
     <div>
       <h1>Details for {item.name || item.aliases}</h1>
-      <p>ID: {item.id}</p>
       <p>Name: {item.name}</p>
       <p>Aliases: {item.aliases}</p>
       {/* Add more details as needed */}
